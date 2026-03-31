5efa2986010974bf133cb1de83cbd232bcd71da2
jd <jason13.deng@gmail.com>
2026-03-31 14:16:31 +0900

    refactor: add rounding up to 5/10 for price

--- services/rakuten/src/services/pricing.ts
+++ services/rakuten/src/services/pricing.ts
@@ -6,12 +6,12 @@ import { categories } from '../config/genres';
 
 // if file exists, postHistoryRaw = file content, empty otherwise
 const configs = JSON.parse(fs.readFileSync(`${process.env.DATA_DIR}/rakuten/config.json`, 'utf-8'));
-const margins = configs.margin
+const margins = configs.markup
 const yenToYuan = configs.YenToYuan
 const shipping = configs.shipping
 
 export function calculatePrice(price:number, category:string) {
     const price_yen = ((price) * (1 + margins[category])) 
-    return price_yen * yenToYuan + shipping[category]
-
+    const price_shipping_yuan = price_yen * yenToYuan + shipping[category]
+    return Math.ceil(price_shipping_yuan/5)*5 
 }
