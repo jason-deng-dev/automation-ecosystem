e646c139124fdd5904f776cf210cdfbe5fedfd87
jd <jason13.deng@gmail.com>
2026-04-03 11:38:05 +0900

    refactor: remove markup from pipeline, delegate to woocommerce plugin

--- services/rakuten/src/services/pricing.ts
+++ services/rakuten/src/services/pricing.ts
@@ -1,15 +1,10 @@
-// read from shared_volume/rakuten/config.json
 import 'dotenv/config';
 import fs from 'fs';
-import { categories } from '../config/genres';
 
-
-// if file exists, postHistoryRaw = file content, empty otherwise
 const configs = JSON.parse(fs.readFileSync(`${process.env.DATA_DIR}/rakuten/config.json`, 'utf-8'));
-const margins = configs.markup
-const yenToYuan = configs.YenToYuan
+const yenToYuan = configs.YenToYuan;
 
-export function calculatePrice(price:number, category:string) {
-    const priceYuan = price * (1 + margins[category]) * yenToYuan
-    return Math.ceil(priceYuan/5)*5
+export function calculatePrice(price: number) {
+    const priceYuan = price * yenToYuan;
+    return Math.ceil(priceYuan / 5) * 5;
 }
