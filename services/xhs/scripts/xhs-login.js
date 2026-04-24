78924ec3d266f8b7847850dad1eb48bca183139f
jd <jason13.deng@gmail.com>
2026-04-24 12:20:08 +0900

    fix: reauth xhs

--- services/xhs/scripts/xhs-login.js
+++ services/xhs/scripts/xhs-login.js
@@ -31,13 +31,17 @@ const timeoutHandle = setTimeout(async () => {
 	process.exit(1);
 }, 5 * 60 * 1000);
 
-await page.goto('https://creator.xiaohongshu.com/publish/publish', { waitUntil: 'commit' });
+await page.goto('https://www.xiaohongshu.com', { waitUntil: 'commit' });
+await page.waitForTimeout(4000);
+if (await page.locator('.login-container').isVisible()) {
+	await page.locator('.login-container').waitFor({ state: 'hidden', timeout: 5 * 60 * 1000 });
+}
 
-try {
-	await page.locator('.login-box-container').waitFor({ state: 'visible', timeout: 20000 });
+await page.goto('https://creator.xiaohongshu.com/publish/publish', { waitUntil: 'commit' });
+await page.waitForTimeout(4000);
+if (await page.locator('.login-box-container').isVisible()) {
+	await page.locator('.login-box-container img').click();
 	await page.locator('.login-box-container').waitFor({ state: 'hidden', timeout: 5 * 60 * 1000 });
-} catch {
-	// already logged in or different auth flow
 }
 
 clearInterval(screenshotInterval);
