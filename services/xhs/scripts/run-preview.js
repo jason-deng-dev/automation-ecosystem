12140074453150412fb1483c801c0e9f0fad7aaf
jd <jason13.deng@gmail.com>
2026-04-24 11:05:34 +0900

    feat(xhs): archive post on preview run

--- services/xhs/scripts/run-preview.js
+++ services/xhs/scripts/run-preview.js
@@ -1,5 +1,23 @@
 import { generatePost } from "../src/generator.js";
-const type = process.argv[2] ? process.argv[2]:'race';
+import { insertPostArchive } from "../src/db/queries.js";
+
+const type = process.argv[2] ? process.argv[2] : 'race';
 
 const post = await generatePost(type);
+
+await insertPostArchive({
+	postType: post.post_type,
+	raceName: post.race_name,
+	title: post.title,
+	hook: post.hook,
+	contents: post.contents,
+	cta: post.cta,
+	description: post.description,
+	hashtags: post.hashtags,
+	comments: post.comments,
+	inputTokens: post.input_tokens,
+	outputTokens: post.output_tokens,
+	published: false,
+});
+
 console.log(JSON.stringify(post));
